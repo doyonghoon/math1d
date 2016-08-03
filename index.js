dd494415b7d8c904e8c0636aa424d14fc6c5d86f
SpencerDo <magicdo7@gmail.com>
2016-08-02 21:38:45 -0700

    mvp

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var cheerio = require('cheerio');
+var request = require('request');
 var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
@@ -10,8 +12,25 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index')
+app.get('/', function(req, res) {
+  request
+  .post('https://www.deanza.edu/schedule/opcourselist.html',
+  {
+    form: {
+      course: 'MATH',
+      WList: 'All'
+    }
+  }, function(err, httpRes, body) {
+    var $ = cheerio.load(body);
+    var table = $('div.da-contents-body table');
+    var hasTaylorBool = hasTaylor($, table.children());
+    var titleText = hasTaylorBool ? 'MATH 1D is open!' : 'MATH 1D is not yet available';
+    var subtitleText = hasTaylorBool ? "Go get him now!" : "";
+    res.render('pages/index', {
+      title: titleText,
+      subtitle: subtitleText
+    });
+  });
 });
 
 app.get('/cool', function(request, response) {
@@ -21,3 +40,17 @@ app.get('/cool', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+var hasTaylor = function(c, table) {
+  var len = 0;
+  if (table) {
+    table.each(function(i, item) {
+      var line = c(this).text();
+      if (line.includes('MATH001D')) {
+        len++;
+        console.log(line);
+      }
+    });
+  }
+  return len > 3;
+}
